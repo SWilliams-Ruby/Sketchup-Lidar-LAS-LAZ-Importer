529ab190a4a54f62852c789f399062b1e4d9d24f
SWilliams-Ruby <88683212+SWilliams-Ruby@users.noreply.github.com>
2021-10-01 13:02:47 -0400

    Delete src/sw_LASimporter directory

--- src/sw_LASimporter/loader.rb
+++
@@ -1,7 +0,0 @@
-require File.join(SW::LASimporter::PLUGIN_DIR, 'options.rb')
-require File.join(SW::LASimporter::PLUGIN_DIR, 'public_header.rb')
-require File.join(SW::LASimporter::PLUGIN_DIR, 'public_header_classes.rb')
-require File.join(SW::LASimporter::PLUGIN_DIR, 'las_file.rb')
-require File.join(SW::LASimporter::PLUGIN_DIR, 'progress_bar_basic_las.rb')
-require File.join(SW::LASimporter::PLUGIN_DIR, 'main.rb')
-
